6046279a3dd1432a1de4237031371c2e23ef6c30
Mike Aldred <mike.aldred@luminousmonkey.org>
2014-07-10 11:45:07 +0800

    Few updates to Lein, and switched to uxterm.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-cljsbuild "1.0.1"]
                   [lein-ancient "0.5.4"]
-                  [lein-midje "3.1.1"]]}}
+                  [lein-midje "3.1.1"]
+                  [lein-kibit "0.0.8"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
