e9ddecd997d559efcdc311279be899a39a2c469b
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-01-20 21:11:40 +0800

    Updated profiles and Weechat.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,4 +3,4 @@
                   [lein-ancient "0.5.4"]
                   [lein-midje "3.1.1"]
                   [lein-kibit "0.0.8"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.8.2-SNAPSHOT"]]}}
