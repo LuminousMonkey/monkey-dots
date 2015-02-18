9e409bf91439059e091c54821bbaa5e2519c9a03
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-02-18 12:14:57 +0800

    Updated lein profiles, offlineimap, weechat.
    Added old Gmail account to offlineimap.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-cljsbuild "1.0.1"]
-                  [lein-ancient "0.5.4"]
+                  [lein-ancient "0.6.2"]
                   [lein-midje "3.1.1"]
                   [lein-kibit "0.0.8"]
-                  [cider/cider-nrepl "0.8.2-SNAPSHOT"]]}}
+                  [lein-typed "0.3.5"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]}}
