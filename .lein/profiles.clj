2009be502f953b86acfff750f2ffed6d03b558f8
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-12-01 11:15:11 +0800

    Updated Lein plugins.
    Updated offlineimap directories.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-cljsbuild "1.0.1"]
-                  [lein-ancient "0.6.2"]
-                  [lein-midje "3.1.1"]
-                  [lein-kibit "0.0.8"]
+{:user {:plugins [[lein-pprint "1.1.2"]
+                  [lein-cljsbuild "1.1.1"]
+                  [lein-ancient "0.6.8"]
+                  [lein-midje "3.2"]
+                  [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+                  [cider/cider-nrepl "0.9.1"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
