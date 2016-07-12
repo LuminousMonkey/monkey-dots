5f751fc1e8b75713a2b9c673fde48ce27bbda2dc
Mike Aldred <mike.aldred@luminousmonkey.org>
2016-07-12 10:42:54 +0800

    Updated.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,6 @@
                   [lein-midje "3.2"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [cider/cider-nrepl "0.10.2"]
-                  [refactor-nrepl "2.0.0"]]
+                  [cider/cider-nrepl "0.13.0-SNAPSHOT"]
+                  [refactor-nrepl "2.2.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
