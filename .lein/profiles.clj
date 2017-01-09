f34d9f8f7620361688536c56cbbb4b34e471672c
Mike Aldred <mike.aldred@luminousmonkey.org>
2017-01-09 13:46:56 +0800

    Updated Lein plugin versions.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:plugins [[lein-pprint "1.1.2"]
-                  [lein-cljsbuild "1.1.3"]
+                  [lein-cljsbuild "1.1.5"]
                   [lein-ancient "0.6.10"]
-                  [lein-midje "3.2"]
-                  [lein-kibit "0.1.2"]
+                  [lein-midje "3.2.1"]
+                  [lein-kibit "0.1.3"]
                   [lein-typed "0.3.5"]
-                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
