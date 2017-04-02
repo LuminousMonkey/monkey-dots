b4a985831c869b75899c7d239cb5d3bb9099e1d0
Mike Aldred <mike.aldred@luminousmonkey.org>
2017-04-03 07:44:11 +0800

    Added some more lein plugins.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,8 @@
                   [lein-midje "3.2.1"]
                   [lein-kibit "0.1.3"]
                   [lein-typed "0.3.5"]
+                  [com.jakemccrary/lein-test-refresh "0.19.0"]
                   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.3.0"]
+                  [jonase/eastwood "0.2.3"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
