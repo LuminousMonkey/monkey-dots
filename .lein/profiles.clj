f4a87b665f31c2156377be6ae0bfde61963f23f4
Mike Aldred <mike.aldred@luminousmonkey.org>
2017-05-19 22:32:55 +0800

    Updated terminator config.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-pprint "1.1.2"]
                   [lein-cljsbuild "1.1.5"]
-                  [lein-ancient "0.6.10"]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.3"]
                   [com.jakemccrary/lein-test-refresh "0.19.0"]
                   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
