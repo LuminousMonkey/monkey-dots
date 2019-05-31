33b91c909079675e8427770aa79e5d396faa8353
Mike Aldred <mike.aldred@luminousmonkey.org>
2019-05-31 15:25:15 +0800

    Lein profile update.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,9 +2,7 @@
                   [lein-cljsbuild "1.1.7"]
                   [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.6"]
-                  [com.jakemccrary/lein-test-refresh "0.22.0"]
-                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [jonase/eastwood "0.2.6"]
-                  [lein-license "0.1.6"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.23.0"]
+                  [jonase/eastwood "0.2.9"]
+                  [lein-license "0.1.8"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
--- .offlineimap.py
+++ .offlineimap.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import keyring
 
-def get_keychain_pass(account=None, server=None):
+def get_keychain_pass(server=None, account=None):
     return keyring.get_password(server, account)
-
