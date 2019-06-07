5242a505710e82aefdfafb3955abd14a99d5218f
Mike Aldred <mike.aldred@luminousmonkey.org>
2019-06-07 22:19:08 +0800

    Lein profile changes.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,7 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
-                  [lein-cljsbuild "1.1.7"]
                   [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
+                  [cider/cider-nrepl "0.22.0-beta2"]
                   [lein-kibit "0.1.6"]
-                  [com.jakemccrary/lein-test-refresh "0.24.1"]
-                  [cider/cider-nrepl "0.21.1"]
-                  [refactor-nrepl "2.4.0"]
                   [jonase/eastwood "0.3.5"]
                   [lein-license "0.1.8"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
- :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
