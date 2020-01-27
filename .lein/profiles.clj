70abf1094d5d9931d66921fb43ab6148488a4cd3
Mike Aldred <mike.aldred@luminousmonkey.org>
2020-01-27 16:01:59 +0800

    Updates Python path on OSX

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
-                  [cider/cider-nrepl "0.22.0-beta2"]]
+                  [cider/cider-nrepl "0.22.0-beta8"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
