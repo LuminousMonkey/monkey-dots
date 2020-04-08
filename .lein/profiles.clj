8773d487583ff4744af30214f87f50503a2e0c6d
Mike Aldred <mike.aldred@luminousmonkey.org>
2020-04-08 13:18:40 +0800

    Updates Terminator config and Lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
-                  [cider/cider-nrepl "0.22.0-beta2"]]
+                  [cider/cider-nrepl "0.22.0-beta2"]
+                  [lein-kibit "0.1.7"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
