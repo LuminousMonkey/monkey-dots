5f70413747a2cf18a664f3fe3471e69408b3f988
Mike Aldred <mike.aldred@luminousmonkey.org>
2020-09-16 13:34:31 +0800

    Clojure updates.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
-                  [cider/cider-nrepl "0.23.0"]]
+                  [cider/cider-nrepl "0.23.0"]
                   [lein-kibit "0.1.7"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
